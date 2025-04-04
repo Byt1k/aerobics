9fb0f602b2e1aa69cd3e9387b19c5aeadb9d76ea
Kirill Budkevich <byt1kip82@gmail.com>
2025-04-04 12:20:24 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src'),
         },
     },
-    // esbuild: {
-    //     jsxInject: `import React from 'react'`,
-    // },
+    preview: {
+        allowedHosts: ['aerobic-competition.ru']
+    }
 })
